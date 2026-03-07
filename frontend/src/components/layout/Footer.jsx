870549fe10b09a7396f4e4dcdb32636662ba2d2e
Artem <artemmochalov445@icloud.com>
2026-03-06 21:37:38 -0500

    minor changes

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="bg-gray-800 text-white text-center py-4">
+    <footer className="bg-dark text-white text-center py-4">
       <p>© 2025 KVK Realty Group</p>
     </footer>
   )
