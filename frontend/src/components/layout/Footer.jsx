40a0a4eefbb32740a2a93fe3b0761d5abf8c4cde
Artem <artemmochalov445@icloud.com>
2026-03-12 18:43:10 -0400

    added equal housing opportunity logo

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -16,7 +16,7 @@ function Footer() {
       <div className="text-center mb-8">
         <p className="text-[10px] xs:text-sm tracking-widest text-gold leading-none">THE</p>
         <p className="text-base xs:text-xl font-serif tracking-widest font-semibold leading-tight text-white">KVK REALTY GROUP</p>
-        <p className="text-[10px] xs:text-sm tracking-widest text-gold mt-0.5">EST. 2024</p>
+        <p className="text-[10px] xs:text-sm tracking-widest text-gold mt-0.5">EST. 2017</p>
       </div>
 
       {/* Розділювач */}
@@ -45,14 +45,19 @@ function Footer() {
           <p className="text-white/50 text-xs sm:text-sm font-sans">248-212-3333</p>
           <p className="text-white/50 text-xs sm:text-sm font-sans text-center break-all">www.kvkrealtygroup.com</p>
           <p className="text-white/50 text-xs sm:text-sm font-sans text-center">Metro Detroit Area, Michigan</p>
+          {/* Equal Housing Opportunity */}
+          <div>
+            <img className="w-[100px]" src="../src/assets/img/Footer/equal-housing-logowhite-1000.png" alt="" />
+          </div>
         </div>
 
       </div>
 
       {/* Нижня лінія */}
-      <div className="border-t border-white/10 pt-6 text-center">
+      <div className="border-t border-white/10 pt-6 flex flex-col sm:flex-row items-center justify-center gap-4">
+      
         <p className="text-white/30 text-xs tracking-widest font-sans uppercase">
-          © 2024 KVK Realty Group — All Rights Reserved
+          © 2017 KVK Realty Group — All Rights Reserved — Equal Housing Opportunity
         </p>
       </div>
 
